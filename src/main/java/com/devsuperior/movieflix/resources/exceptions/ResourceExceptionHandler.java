e515d2d467c0ef33619f769825d2579108ba6c81
luanalimadeveloper <luanalima.developer@gmail.com>
2022-06-08 15:25:54 -0300

    exception

--- src/main/java/com/devsuperior/movieflix/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/movieflix/resources/exceptions/ResourceExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.devsuperior.movieflix.resources.exceptions;
 
-import com.devsuperior.dslearnbds.services.exceptions.DatabaseException;
-import com.devsuperior.dslearnbds.services.exceptions.ForbiddenException;
-import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
-import com.devsuperior.dslearnbds.services.exceptions.UnauthorizedException;
+import com.devsuperior.movieflix.services.exceptions.DatabaseException;
+import com.devsuperior.movieflix.services.exceptions.ForbiddenException;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
